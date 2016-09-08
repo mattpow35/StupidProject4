fb4e58427602db1e17f9503b5514c52bc6bc8cf7
Matt Powley <mattpow35@gmail.com>
2016-09-08 07:44:05 -0600

    Created 4th stupid project. Just like the third but I did it on my own.

---
+++ src/stupid/control/Stupid4controller.java
@@ -0,0 +1,8 @@
+package stupid.control;
+
+public class Stupid4controller {
+	public void start()
+	{
+		System.out.println("I did this on my own. It is a very stupid project");
+	}
+}
---
+++ src/stupid/control/Stupid4runner.java
@@ -0,0 +1,10 @@
+package stupid.control;
+
+public class Stupid4runner {
+	public static void main (String [] args)
+	{
+		Stupid4controller firstAppController = new Stupid4controller();
+		firstAppController.start();
+	}
+
+}
